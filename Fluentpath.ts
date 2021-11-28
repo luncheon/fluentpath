90d7f2ba885b29a413ce8a03db729a05da5cd3f6
luncheon <takeuck+github@gmail.com>
2021-11-28 19:52:11 +0900

    no comment

--- Fluentpath.ts
+++ Fluentpath.ts
@@ -3,6 +3,9 @@ export interface FluentpathOptions {
   readonly distanceThreshold: number;
 }
 
+const F = (ss: readonly string[], ...values: readonly number[]) =>
+  values.map((value, i) => ss[i] + value.toFixed(3)).join('') + ss[ss.length - 1];
+
 export class Fluentpath {
   private readonly lastNPointers: DOMPoint[] = [];
   readonly points: [number, number][] = [];
@@ -20,7 +23,7 @@ export class Fluentpath {
     const { lastNPointers, points } = this;
     lastNPointers.unshift(point);
     if (lastNPointers.length === 1) {
-      this.d = `M${point.x} ${point.y}v0`;
+      this.d = F`M${point.x} ${point.y}v0`;
       points.push([point.x, point.y]);
     } else {
       lastNPointers.length > this.smoothingPointCount && lastNPointers.pop();
@@ -30,16 +33,16 @@ export class Fluentpath {
       const distance = Math.hypot(x1 - x, y1 - y);
       if (distance > this.distanceThreshold) {
         if (points.length === 1) {
-          this._d = `M${x1} ${y1}`;
+          this._d = F`M${x1} ${y1}`;
         } else if (points.length > 3) {
           const [x3, y3] = points[points.length - 3]!;
           const [x2, y2] = points[points.length - 2]!;
-          const a1 = Math.atan2(y2 - y3, x2 - x3);
-          const a2 = Math.atan2(y1 - y, x1 - x);
           const r = Math.hypot(x1 - x2, y1 - y2) * 0.15;
-          this._d += `C${x2 + r * Math.cos(a1)} ${y2 + r * Math.sin(a1)} ${x1 + r * Math.cos(a2)} ${y1 + r * Math.sin(a2)} ${x1} ${y1}`;
+          const r2 = r / Math.hypot(x2 - x3, y2 - y3);
+          const r1 = r / Math.hypot(x1 - x, y1 - y);
+          this._d += F`C${x2 + (x2 - x3) * r2} ${y2 + (y2 - y3) * r2} ${x1 + (x1 - x) * r1} ${y1 + (y1 - y) * r1} ${x1} ${y1}`;
         }
-        this.d = this._d + `L${x} ${y}`;
+        this.d = this._d + F`L${x} ${y}`;
         points.push([x, y]);
       }
     }
